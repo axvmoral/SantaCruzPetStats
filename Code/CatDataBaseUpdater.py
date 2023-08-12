e401cc8dc52b1ba8a698d296567dafab89665bb8
Axel Morales <axvmoral@ucsc.edu>
2023-08-12 16:58:31 -0700

    Update CatDataBaseUpdater.py

--- Code/CatDataBaseUpdater.py
+++ Code/CatDataBaseUpdater.py
@@ -9,6 +9,7 @@ from textblob import TextBlob
 import sqlite3
 
 st = datetime.now()
+print('Retrieving Web Data...')
 matches = get_matches(type='lost.cat')
 url = f'https://petharbor.com/results.asp?searchtype=LOST&start=4&friends=1&samaritans=1&nosuccess=0&rows={matches}&imght=120&imgres=Detail&tWidth=200&view=sysadm.v_sncr&nobreedreq=1&bgcolor=ffffff&text=000000&fontface=arial&fontsize=10&col_hdr_bg=c0c0c0&SBG=c0c0c0&miles=20&shelterlist=%27SNCR%27,%27SNCR1%27&atype=&where=type_CAT&PAGE=1'
 df = pd.read_html(
@@ -35,31 +36,55 @@ df.columns = ['InfoLink', 'ID', 'Name', 'Gender', 'Color', 'Breed', 'Age', 'Weig
               'EnterDate', 'Comment', 'Polarity', 'InGroup']
 df['Treated'] = df.apply(lambda x: get_treatment(x['Gender']), axis=1)
 df['RawGender'] = df.apply(lambda x: get_raw_gender(x['Gender']), axis=1)
+today = datetime.today().strftime('%Y-%m-%d')
+df['FirstWeb'] = today
+df['LastDate'] = None
 print('Web Data Succesfully Retrieved')
+print('Updating Data Base...')
 con = sqlite3.connect('data/LostCatDataBase.db')
 cur = con.cursor()
 cur.execute(
     '''
     CREATE TABLE IF NOT EXISTS
-    WebData(ID PRIMARY KEY, InfoLink TEXT, Name TEXT, Gender TEXT,
+    WebData(ID PRIMARY KEY, Name TEXT, Gender TEXT,
     Color TEXT, Breed TEXT, Age DECIMAL, Weight DECIMAL, Located TEXT,
     EnterDate TEXT, Comment TEXT, Polarity REAL, InGroup INTEGER,
-    Treated INTEGER, RawGender INTEGER, FirstWeb TEXT)
+    Treated INTEGER, RawGender INTEGER, FirstWeb TEXT, LastDate TEXT)
     '''
     )
-NewWebDataIDs = []
+cur.execute(
+    '''
+    CREATE TABLE IF NOT EXISTS
+    NewWebData(ID PRIMARY KEY, Name TEXT, Gender TEXT,
+    Color TEXT, Breed TEXT, Age DECIMAL, Weight DECIMAL, Located TEXT,
+    EnterDate TEXT, Comment TEXT, Polarity REAL, InGroup INTEGER,
+    Treated INTEGER, RawGender INTEGER, FirstWeb TEXT, LastDate TEXT)
+    '''
+    )
 for i in range(len(df)):
-    cur.execute('SELECT EXISTS(SELECT 1 FROM WebData WHERE ID = ?)', (df['ID'][i], ))
-    if cur.fetchone():
-        NewWebDataIDs.append(df['ID'][i])
-NewWebData = pd.DataFrame(data={'ID': NewWebDataIDs}).merge(df, how='inner', on='ID')
-NewWebData['FirstWeb'] = datetime.today().strftime('%Y-%m-%d')
-for i in range(len(NewWebData)):
+    cur.execute(
+        '''
+        INSERT OR IGNORE INTO NewWebData VALUES
+        (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        ''', df.iloc[i,1:]
+    )
+cur.execute(
+    '''
+    SELECT WebData.ID FROM WebData OUTER LEFT JOIN NewWebData ON
+    WebData.ID = NewWebData.ID
+    '''
+    )
+NotOnWebSite = cur.fetchall()
+for entry in NotOnWebSite:
+    cur.execute('''
+UPDATE WebData SET LastDate = ? WHERE ID = ?
+''', (today, entry[0],))
+for i in range(len(df)):
     cur.execute(
         '''
         INSERT OR IGNORE INTO WebData VALUES
         (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-        ''', NewWebData.iloc[i,:]
+        ''', df.iloc[i,1:]
     )
 con.commit()
 con.close()
