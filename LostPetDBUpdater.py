f68d5a0c8d4fdbfaed642ca706dfc42681c6c721
Axel Morales <axvmoral@ucsc.edu>
2023-08-15 08:39:12 -0700

    Created New DB

--- LostPetDBUpdater.py
+++ LostPetDBUpdater.py
@@ -6,8 +6,9 @@ import ssl
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-def UpdateLostPetDB(pet: str, db: str) -> None:
+def UpdateLostPetDB(pet: str, db: str, table) -> None:
     lst = datetime.now()
+    print(f'Commencing {table} Table Update...')
     print('Retrieving Web Data...')
     MATCHES = get_matches(type=pet)
     url = f'https://petharbor.com/results.asp?searchtype=LOST&start=4&friends=1&samaritans=1&nosuccess=0&rows={MATCHES}&imght=120&imgres=Detail&tWidth=200&view=sysadm.v_sncr&nobreedreq=1&bgcolor=ffffff&text=000000&fontface=arial&fontsize=10&col_hdr_bg=c0c0c0&SBG=c0c0c0&miles=20&shelterlist=%27SNCR%27,%27SNCR1%27&atype=&where=type_{pet}&PAGE=1'
@@ -39,7 +40,7 @@ def UpdateLostPetDB(pet: str, db: str) -> None:
     df['FirstWeb'] = TODAY
     df['LastDate'] = None
     print('Web Data Succesfully Retrieved')
-    print('Updating Data Base...')
+    print(f'Updating {table}...')
     VALUES = '(?' + ',?' * (len(df.columns) - 2)  + ')'
     con = sqlite3.connect(db)
     cur = con.cursor()
@@ -50,23 +51,27 @@ def UpdateLostPetDB(pet: str, db: str) -> None:
                 Treated INTEGER, RawGender INTEGER, FirstWeb TEXT, LastDate TEXT)
                 ''')
     cur.executemany(f'INSERT INTO NewWebData VALUES{VALUES}', (row[1] for row in df.iloc[:,1:].iterrows()))
-    cur.execute('SELECT WebData.ID FROM WebData LEFT JOIN NewWebData ON WebData.ID = NewWebData.ID WHERE NewWebData.ID IS NULL AND WebData.LastDate IS NULL')
-    cur.executemany(f'UPDATE WebData SET LastDate = "{TODAY}" WHERE ID = ?', cur.fetchall())
-    cur.execute('INSERT INTO WebData SELECT NewWebData.* FROM NewWebData LEFT JOIN WebData ON NewWebData.ID = WebData.ID WHERE WebData.ID IS NULL')
+    cur.execute(f'SELECT {table}.ID FROM {table} LEFT JOIN NewWebData ON {table}.ID = NewWebData.ID WHERE NewWebData.ID IS NULL AND {table}.LastDate IS NULL')
+    gone = cur.fetchall()
+    print(f'{len(gone)} {pet.lower()}(s) gone from website')
+    cur.executemany(f'UPDATE {table} SET LastDate = "{TODAY}" WHERE ID = ?', gone)
+    cur.execute(f'SELECT NewWebData.* FROM NewWebData LEFT JOIN {table} ON NewWebData.ID = {table}.ID WHERE {table}.ID IS NULL')
+    new = cur.fetchall()
+    print(f'{len(new)} new {pet.lower()}(s) on website')
+    cur.executemany(f'INSERT INTO {table} VALUES{VALUES}', new)
     cur.execute('DROP TABLE NewWebData')
     con.commit()
     con.close()
-    print('Data Base Succesfully Updated')
+    print(f'{table} Succesfully Updated')
     let = datetime.now()
-    print(f'Time Elapsed: {let - lst}')
+    print(f'Time Elapsed: {let - lst}\n')
     return None
 
 if __name__ == '__main__':
     gst = datetime.now()
-    print('Commencing Lost Cat Data Base Update...')
-    UpdateLostPetDB(pet='CAT', db='data/LostCatDataBase.db')
-    print('\n')
-    print('Commencing Lost Dog Data Base Update...')
-    UpdateLostPetDB(pet='DOG', db='data/LostDogDataBase.db')
+    print('Commencing Pets Data Base Update...\n')
+    UpdateLostPetDB(pet='CAT', db='data/Pets.db', table='LostCats')
+    UpdateLostPetDB(pet='DOG', db='data/Pets.db', table='LostDogs')
     get = datetime.now()
+    print('Pets Data Base Succesfully Updated')
     print(f'Total Time Elapsed: {get - gst}')
