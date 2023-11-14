40ecdade421c88343b21da757f4e69473203bbce
Axel Morales <axvmoral@ucsc.edu>
2023-11-14 08:49:34 -0800

    Restructured Project

--- PETS.py
+++ PETS.py
@@ -22,7 +22,7 @@ class PETS:
             for j, ENTRY in enumerate(ROW.find_all("td")):
                 if j == 0:
                     data[HEADERS[j]].append(URL_ROOT + ENTRY.a.get("href"))
-                    data["ImageLink"].append(URL_ROOT + ENTRY.img.get("src"))
+                    data["Image"].append(URL_ROOT + ENTRY.img.get("src"))
                 elif re.search(r"unknown", ENTRY.text, re.IGNORECASE):
                     data[HEADERS[j]].append(None)
                 else:
@@ -30,7 +30,8 @@ class PETS:
         self.DATAFRAME = pd.DataFrame(data).rename({"Picture": "InfoLink"}).set_index(keys="ID")
         return None
 
-    def PrepareData(self, data: DataFrame) -> None:
+    def PrepareData(self, DATA: DataFrame) -> None:
+        data = DATA.copy()
         data.Name = data.Name.map(lambda x: x.strip("*").lower(), na_action="ignore")
         data.Color = data.Color.map(lambda x: x.strip().lower(), na_action="ignore")
         data.Breed = data.Breed.map(lambda x: x.strip().lower(), na_action="ignore")
@@ -51,19 +52,23 @@ class PETS:
         data.Gender = data.Gender.str.contains(r"male", case=False)
         return data
 
-    def UpdateData(self, EXISTING_DATA_PATH: str) -> None:
-        EXISTS = os.path.exists(EXISTING_DATA_PATH)
+    def UpdateData(self, DATA_PATH: str) -> None:
+        EXISTS = os.path.exists(DATA_PATH)
         if EXISTS:
-            EXISTING_DATA = pd.read_csv(EXISTING_DATA_PATH, index_col="ID")
-            INTERSECTING_INDEX = EXISTING_DATA.index.intersection(self.DATAFRAME.index)
-            NEW_DATA = self.PrepareData(self.DATAFRAME[INTERSECTING_INDEX])
+            EXISTING_DATA = pd.read_csv(DATA_PATH, usecols="ID", index_col="ID")
+            DIFFERENCE_INDEX = self.DATAFRAME.index.difference(EXISTING_DATA.index)
+            NEW_DATA = self.PrepareData(self.DATAFRAME[DIFFERENCE_INDEX])
         else:
-            NEW_DATA = self.DATAFRAME
-        NEW_DATA.to_csv(EXISTING_DATA_PATH, mode="a", header=EXISTS)
+            DIRNAME = os.path.dirname(DATA_PATH)
+            if not os.path.exists(DIRNAME):
+                os.mkdir(DIRNAME)
+            NEW_DATA = self.PrepareData(self.DATAFRAME)
+        NEW_DATA.reset_index().to_csv(DATA_PATH, mode="a", header= not EXISTS, index=False)
         return None
 
 
 if __name__ == "__main__":
     URL = "https://petharbor.com/results.asp?searchtype=LOST&start=4&friends=1&samaritans=1&nosuccess=0&rows=139&imght=120&imgres=detail&tWidth=200&view=sysadm.v_sncr&nobreedreq=1&bgcolor=ffffff&text=000000&fontface=arial&fontsize=10&col_hdr_bg=c0c0c0&SBG=c0c0c0&miles=20&shelterlist=%27SNCR%27,%27SNCR1%27&atype=&where=type_CAT&PAGE=1"
     pets = PETS(URL)
-    d = pets.PrepareData(pets.DATAFRAME)
+    pets.UpdateData("Datasets/lost_cats.csv")
+    df = pd.read_csv("Datasets/lost_cats.csv")
