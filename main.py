58dd9a1994e693f0be62a34be6705e76fa0473d7
Axel Morales <axvmoral@ucsc.edu>
2024-10-06 11:11:18 -0700

    Update

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-""" Module to run daily. """
+import pets
 
-__author__ = "Axel V. Morales Sanchez"
+database_path = "pets.db"
 
-if __name__ == "__main__":
-    with open("get_links.py") as file:
-        exec(file.read())
+pets.update_database(
+    lost_url="https://petharbor.com/results.asp?searchtype=LOST&start=4&friends=1&samaritans=1&nosuccess=0&rows=10&imght=120&imgres=detail&tWidth=200&view=sysadm.v_sncr&nobreedreq=1&bgcolor=ffffff&text=000000&fontface=arial&fontsize=10&col_hdr_bg=c0c0c0&SBG=c0c0c0&miles=20&shelterlist=%27SNCR%27,%27SNCR1%27&atype=&where=type_CAT&PAGE=1",
+    adoptable_url="https://petharbor.com/results.asp?searchtype=ADOPT&friends=1&samaritans=1&nosuccess=0&rows=25&imght=120&imgres=thumb&view=sysadm.v_sncr&bgcolor=white&text=black&link=blue&alink=purple&vlink=purple&fontface=arial&fontsize=10&col_hdr_bg=silver&col_hdr_fg=black&col_bg=white&col_fg=black&SBG=silver&miles=200&shelterlist=%27SNCR%27,%27SNCR1%27&atype=&where=type_CAT&PAGE=1",
+    database_path=database_path,
+    table="cats"
+)
 
-    with open("update_database.py") as file:
-        exec(file.read())
+pets.update_database(
+    lost_url="https://petharbor.com/results.asp?searchtype=LOST&start=4&friends=1&samaritans=1&nosuccess=0&rows=10&imght=120&imgres=detail&tWidth=200&view=sysadm.v_sncr&nobreedreq=1&bgcolor=ffffff&text=000000&fontface=arial&fontsize=10&col_hdr_bg=c0c0c0&SBG=c0c0c0&miles=20&shelterlist=%27SNCR%27,%27SNCR1%27&atype=&where=type_DOG&PAGE=1",
+    adoptable_url="https://petharbor.com/results.asp?searchtype=ADOPT&friends=1&samaritans=1&nosuccess=0&rows=25&imght=120&imgres=thumb&view=sysadm.v_sncr&bgcolor=white&text=black&link=blue&alink=purple&vlink=purple&fontface=arial&fontsize=10&col_hdr_bg=silver&col_hdr_fg=black&col_bg=white&col_fg=black&SBG=silver&miles=200&shelterlist=%27SNCR%27,%27SNCR1%27&atype=&where=type_DOG&PAGE=1",
+    database_path=database_path,
+    table="dogs"
+)
+
+pets.update_database(
+    lost_url="https://petharbor.com/results.asp?searchtype=LOST&start=4&friends=1&samaritans=1&nosuccess=0&rows=10&imght=120&imgres=detail&tWidth=200&view=sysadm.v_sncr&nobreedreq=1&bgcolor=ffffff&text=000000&fontface=arial&fontsize=10&col_hdr_bg=c0c0c0&SBG=c0c0c0&miles=20&shelterlist=%27SNCR%27,%27SNCR1%27&atype=&where=type_OO&PAGE=1",
+    adoptable_url="https://petharbor.com/results.asp?searchtype=ADOPT&friends=1&samaritans=1&nosuccess=0&rows=25&imght=120&imgres=thumb&view=sysadm.v_sncr&bgcolor=white&text=black&link=blue&alink=purple&vlink=purple&fontface=arial&fontsize=10&col_hdr_bg=silver&col_hdr_fg=black&col_bg=white&col_fg=black&SBG=silver&miles=200&shelterlist=%27SNCR%27,%27SNCR1%27&atype=&where=type_OO&PAGE=1",
+    database_path=database_path,
+    table="others"
+)
